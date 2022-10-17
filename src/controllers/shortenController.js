aa9a3dd1590955486f7b73f798a99286458b0fd0
Lucas <lucasfelicio378@email.com>
2022-10-17 05:55:13 -0300

    delet complete

--- src/controllers/shortenController.js
+++ src/controllers/shortenController.js
@@ -36,7 +36,7 @@ export async function getShortenUrl(req, res) {
     `SELECT * FROM encurtados WHERE "shortUrl" = $1;`,
     [req.params.shortUrl]
   );
-  if (encurtados.rows.length === 0) return res.sendStatus(401);
+  if (encurtados.rows.length === 0) return res.sendStatus(404);
 
   await connection.query(
     `UPDATE encurtados SET visits = $1 WHERE "shortUrl" = $2;`,
@@ -44,4 +44,39 @@ export async function getShortenUrl(req, res) {
   );
 
   res.redirect(encurtados.rows[0].url);
+}
+
+export async function deleteShorten(req, res) {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  const urlId = req.params.id;
+
+  const verifyToken = await connection.query(
+    `SELECT * FROM logados WHERE token LIKE $1;`,
+    [token]
+  );
+  if (verifyToken.rows.length === 0) return res.sendStatus(401);
+  console.log(100);
+
+  const userId = verifyToken.rows[0].userId;
+  console.log(userId);
+
+  const verifyUrl = await connection.query(
+    `SELECT * FROM encurtados WHERE id = $1 AND "userId" = $2;`,
+    [urlId, userId]
+  );
+  if (verifyUrl.rows.length === 0) {
+    const verifyUrlExiste = await connection.query(
+      `SELECT * FROM encurtados WHERE WHERE id = $1;`,
+      [urlId]
+    );
+    if (verifyUrlExiste.rows.length === 0) return res.sendStatus(404);
+    else return res.sendStatus(401);
+  }
+  console.log(200);
+
+  await connection.query(
+    `DELETE FROM encurtados WHERE id = $1 AND "userId" = $2`,
+    [urlId, userId]
+  );
+  res.sendStatus(204);
 }
--- src/routers/shortenRouter.js
+++ src/routers/shortenRouter.js
@@ -4,6 +4,7 @@ import {
   postShorten,
   getShortenId,
   getShortenUrl,
+  deleteShorten,
 } from "../controllers/shortenController.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/urls/shorten", validatePostShorten, postShorten);
 router.get("/urls/:id", getShortenId);
 router.get("/urls/open/:shortUrl", getShortenUrl);
+router.delete("/urls/:id", deleteShorten);
 
 export default router;
