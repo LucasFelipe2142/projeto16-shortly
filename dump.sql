b6c44eb5a588282e210fc9ee9d430273d17438d9
Lucas <lucasfelicio378@email.com>
2022-10-17 16:15:16 -0300

    remove bug in dump

--- dump.sql
+++ dump.sql
@@ -5,21 +5,21 @@ CREATE TABLE "users" (
 	name TEXT NOT NULL,
     email TEXT NOT NULL,
     password TEXT NOT NULL,
-    "createdAt" TIMESTAMP DEFAULT NOW
+    "createdAt" TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE "logados" (
 	id SERIAL PRIMARY KEY,
 	"userId" INTEGER REFERENCES "users"("id"),
     token TEXT NOT NULL,
-    "createdAt" TIMESTAMP DEFAULT NOW
+    "createdAt" TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE "encurtados" (
 	id SERIAL PRIMARY KEY,
-	"userId" TEXT NOT NULL,
+	"shortUrl" TEXT NOT NULL,
     url TEXT NOT NULL,
     "userId" INTEGER REFERENCES "users"("id"),
     visits INTEGER NOT NULL,
-    "createdAt" TIMESTAMP DEFAULT NOW
+    "createdAt" TIMESTAMP DEFAULT NOW()
 );
